b3143ec7b07d0629f31aa490d545f034fac9e44d
eyuleo <eyuleo10@gmail.com>
2025-03-10 10:49:55 +0300

    add logging and error handling middleware

--- bloglist/app.js
+++ bloglist/app.js
@@ -2,24 +2,29 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import config from "./utils/config.js"
+import logger from "./utils/logger.js"
+import middleware from "./utils/middleware.js"
 import blogsRouter from "./controllers/blogs.js"
 
 const app = express()
 
 mongoose.set("strictQuery", false)
 const mongoUrl = config.MONGODB_URI
-console.log("connecting to", mongoUrl)
+logger.info("connecting to", mongoUrl)
 mongoose
 	.connect(mongoUrl)
 	.then(() => {
-		console.log("connected to MongoDB")
+		logger.info("connected to MongoDB")
 	})
 	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message)
+		logger.error("error connecting to MongoDB:", error.message)
 	})
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 export default app
--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -9,12 +9,15 @@ blogsRouter.get("/", (request, response) => {
 	})
 })
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", (request, response, next) => {
 	const blog = new Blog(request.body)
 
-	blog.save().then((result) => {
-		response.status(201).json(result)
-	})
+	blog
+		.save()
+		.then((result) => {
+			response.status(201).json(result)
+		})
+		.catch((error) => next(error))
 })
 
 export default blogsRouter
